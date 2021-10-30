f140832c63f091374ef7b105fca6a79eb819ab79
Ainaz10 <burganov.ainaz@gmail.com>
2021-10-30 23:00:37 +0300

    update few files

--- app/src/main/java/com/example/projecttgu1/MainActivity.kt
+++ app/src/main/java/com/example/projecttgu1/MainActivity.kt
@@ -13,42 +13,43 @@ class MainActivity  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(MY_OWN_TAG, "onCreate(Ты видел деву на скале\n" +
+        Log.d(MY_OWN_TAG, "onCreate(_1_Ты видел деву на скале\n" +
                 "В одежде белой над волнами)")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(MY_OWN_TAG, "onStart(Когда, бушуя в бурной мгле,\n" +
+        Log.d(MY_OWN_TAG, "onStart(_2_Когда, бушуя в бурной мгле,\n" +
                 "Играло море с берегами,)")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(MY_OWN_TAG, "onResume(Когда луч молний озарял\n" +
+        Log.d(MY_OWN_TAG, "onResume(_3_Когда луч молний озарял\n" +
                 "Ее всечасно блеском алым)")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(MY_OWN_TAG, "onPause(И ветер бился и летал\n" +
+        Log.d(MY_OWN_TAG, "onPause(_4_И ветер бился и летал\n" +
                 "С ее летучим покрывалом?)")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(MY_OWN_TAG, "onStop(Прекрасно море в бурной мгле\n" +
+        Log.d(MY_OWN_TAG, "onStop(_5_Прекрасно море в бурной мгле\n" +
                 "И небо в блесках без лазури;)")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(MY_OWN_TAG, "onRestart(Но верь мне: дева на скале)")
+        Log.d(MY_OWN_TAG, "onRestart")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(MY_OWN_TAG, "onDestroy(Прекрасней волн, небес и бури.)")
+        Log.d(MY_OWN_TAG, "_6_onDestroy(Но верь мне: дева на скале\n" +
+                "Прекрасней волн, небес и бури.)")
     }
 
 }
